5809940b9d216ff67cb3edd9f53a1dc2b307f474
Benedikt Fauseweh <Benedikt.fauseweh@gmail.com>
2026-05-25 12:43:02 +0200

    First Files

---
+++ Physlib/CondensedMatter/LiebRobinsonBound/Basic.lean
@@ -0,0 +1,9 @@
+/-
+Copyright (c) 2025 Benedikt Fauseweh. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Benedikt Fauseweh
+-/
+
+module
+
+public import Physlib.QuantumMechanics.FiniteTarget.HilbertSpace
